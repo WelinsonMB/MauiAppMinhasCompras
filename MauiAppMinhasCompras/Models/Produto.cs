6da1d7b3a164d8e65b30a4638fd1933efaf1d5db
WelinsonMB <welinson.mendes2015@hotmail.com>
2025-03-30 16:42:17 -0300

    Adição da validação de dados

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,12 +4,57 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+
+        string _descricao;
+ 
+
         [PrimaryKey, AutoIncrement]
-        public int Id { get; set;  }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
-        public double Total { get => Quantidade * Preco;  }
+        public int Id { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrução");
+                }
+
+                _descricao = value;
+            }
+        }
+
+        private double _quantidade;
+
+        public double Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                if (value == 0) 
+                    {
+                    throw new Exception("Por favor, preencha uma quantidade valída");
+                }
+
+                _quantidade = value; 
+            }
+        }
+
+        private double _preco;
+        public double Preco 
+        { 
+            get => _preco;
+            set 
+            {
+                if (value == 0) 
+                {
+                    throw new Exception("Por favor, preencha um valor válido");
+                }
+
+                _preco = value; 
+            } 
+        }
+        public double Total { get => Quantidade * Preco; }
 
     }
 }
